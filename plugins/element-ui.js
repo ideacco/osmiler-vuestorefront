7c1a5f85fd26768d9949b67571b77ea2cd234e17
曾峻峰 <zjfung@sleepon.cn>
2022-06-14 15:45:49 +0800

    feat:补充图片

--- plugins/element-ui.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import 'element-ui/lib/theme-chalk/index.css'
-import ElementUI from 'element-ui'
-// import axios from 'axios'
-// Vue.prototype.$axios = axios
-
-Vue.use(ElementUI)
