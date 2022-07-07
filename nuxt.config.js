a949958b9d93ccb6cbe1a07a7b73db785e516703
曾峻峰 <zjfung@sleepon.cn>
2022-07-07 15:54:40 +0800

    feat:优化nuxt 性能

--- nuxt.config.js
+++ nuxt.config.js
@@ -169,6 +169,7 @@ const config = {
     '@nuxtjs/i18n',
     'nuxt-facebook-pixel-module',
     '@nuxtjs/gtm',
+    'nuxt-precompress',
     // '@nuxtjs/google-gtag',
     'cookie-universal-nuxt',
     'vue-scrollto/nuxt',
@@ -178,6 +179,40 @@ const config = {
     '@nuxt/image',
     '@nuxtjs/axios',
   ],
+  nuxtPrecompress: {
+    enabled: true, // Enable in production
+    report: false, // set true to turn one console messages during module init
+    test: /\.(js|css|html|txt|xml|svg)$/, // files to compress on build
+    // Serving options
+    middleware: {
+      // You can disable middleware if you serve static files using nginx...
+      enabled: true,
+      // Enable if you have .gz or .br files in /static/ folder
+      enabledStatic: true,
+      // Priority of content-encodings, first matched with request Accept-Encoding will me served
+      encodingsPriority: ['br', 'gzip'],
+    },
+    gzip: {
+      // should compress to gzip?
+      enabled: true,
+      // compression config
+      // https://www.npmjs.com/package/compression-webpack-plugin
+      filename: '[path].gz[query]', // middleware will look for this filename
+      threshold: 10240,
+      minRatio: 0.8,
+      compressionOptions: { level: 9 },
+    },
+    brotli: {
+      // should compress to brotli?
+      enabled: true,
+      // compression config
+      // https://www.npmjs.com/package/compression-webpack-plugin
+      filename: '[path].br[query]', // middleware will look for this filename
+      compressionOptions: { level: 11 },
+      threshold: 10240,
+      minRatio: 0.8,
+    },
+  },
   gtm: {
     id: 'GTM-PB39ZGK', // Used as fallback if no runtime config is provided
     enabled: true,
@@ -412,6 +447,7 @@ const config = {
     },
     build: {
       transpile: [/^@storefront-ui/, /^UIkit/],
+      analyze:true
     },
     workbox: {
       offlineStrategy: 'StaleWhileRevalidate',
