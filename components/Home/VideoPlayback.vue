b280d8846f9663e796a5ca61f6ba55d25302fb8b
曾峻峰 <zjfung@sleepon.cn>
2022-08-04 11:00:34 +0800

    “更换图片”

--- components/Home/VideoPlayback.vue
+++ components/Home/VideoPlayback.vue
@@ -19,7 +19,7 @@
       tm-header-transparent="light"
       uk-height-viewport="offset-top: true;"
       :style="{ marginTop: innerWidth > 940 ? '-120px' : '0px' }"
-      style="height: 1220px;
+      style="height: 960px;
       background-image: url('/wp-content/themes/yootheme/cache/Hero_2.webp')"
     >
 
